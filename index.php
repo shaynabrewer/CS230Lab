7a9ba9534d61c5b95334694e9439596c10b9c4f5
shaynabrewer <slb0088@mix.wvu.edu>
2021-02-05 01:54:02 -0500

    about page

---
+++ index.php
@@ -0,0 +1,50 @@
+<main>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p style= "color: darkred"> Horror Movie Reviews</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/1.jpeg">
+                <div class="container">
+                    <h2>Jason Voorhees</h2>
+                    <p>Camp Counselor</p>
+                    <p>Likes to play tag</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/5.jpeg">
+                <div class="container">
+                    <h2>Billy</h2>
+                    <p>Puppeteer</p>
+                    <p>Likes to make games</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/2.jpeg">
+                <div class="container">
+                    <h2>Dr. Hannibal Lecter</h2>
+                    <p>Psychiatrist</p>
+                    <p>Likes to try new foods</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/4.jpeg">
+                <div class="container">
+                    <h2>Jack Torrance</h2>
+                    <p>Writer / Hotel Caretaker</p>
+                    <p>All work and no play</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
