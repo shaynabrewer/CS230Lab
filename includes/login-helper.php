cf301555268351ccdf4a8f846854014e69a33f18
shaynabrewer <slb0088@mix.wvu.edu>
2021-02-27 21:33:08 -0500

    Lab 5 - Creating profiles

--- includes/login-helper.php
+++ includes/login-helper.php
@@ -35,6 +35,8 @@ if(isset($_POST['login-submit'])){
                 $_SESSION['uname'] = $data['uname'];
 
                 echo "<h1>Success!</h1><p>$uname.</p>";
+                header("Location: ../profile.php?success=login");
+                exit();
             } else {
                 header('Location: ../login.php?error=WrongPass');
                 exit();
